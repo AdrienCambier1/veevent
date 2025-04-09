47a97697af8aa8df39fa904755bdd878391e6632
Cambier-Adrien <adrien.cambier04@gmail.com>
2025-04-09 17:46:57 +0200

    ...

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -13,53 +13,59 @@ export default function Footer() {
   return (
     <footer>
       <div className="container flex flex-col gap-12">
-        <div className="flex flex-col lg:flex-row gap-8 justify-between">
-          <Link
-            href="/"
-            className="logo"
-            onClick={(e) => {
-              if (window.location.pathname === "/") {
-                e.preventDefault();
-                scrollToTop();
-              }
-            }}
-          >
-            v<span>ee</span>vent
-          </Link>
-          <div className="flex flex-row gap-12">
-            <nav className="flex flex-col gap-4 ">
-              <h4>Ressources</h4>
-              <Link href="/activities/events">Evenements</Link>
-              <Link href="/activities/organisers">Organisateurs</Link>
-              <Link href="/cities">Villes</Link>
-              <Link href="/saved/inscriptions">Mes inscriptions</Link>
-              <Link href="/saved/marked">Marqués</Link>
-              <Link href="/subscriptions">Abonnements</Link>
-            </nav>
-            <nav className="flex flex-col gap-4 ">
-              <h4>Gérer son compte</h4>
-              <Link href="">Informations du compte</Link>
-              <Link href="">Mes evenements</Link>
-              <Link href="">Evenements participés</Link>
-              <Link href="">Avis</Link>
-            </nav>
-          </div>
-          <div className="flex flex-col lg:flex-row gap-8">
-            <nav className="flex gap-4">
-              <Link href="">
-                <Instagram />
-              </Link>
-              <Link href="">
-                <Facebook />
-              </Link>
-              <Link href="">
-                <X />
-              </Link>
-            </nav>
-            <Link href="" className="primary-btn">
-              Publie ton événement
-              <ArrowUpRight />
+        <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-8 md:flex-row md:justify-between">
+            <Link
+              href="/"
+              className="logo"
+              onClick={(e) => {
+                if (window.location.pathname === "/") {
+                  e.preventDefault();
+                  scrollToTop();
+                }
+              }}
+            >
+              v<span>ee</span>vent
             </Link>
+            <div className="flex items-center gap-8">
+              <nav className="flex gap-4">
+                <Link href="">
+                  <Instagram />
+                </Link>
+                <Link href="">
+                  <Facebook />
+                </Link>
+                <Link href="">
+                  <X />
+                </Link>
+              </nav>
+              <Link href="" className="primary-btn">
+                Publie ton événement
+                <ArrowUpRight />
+              </Link>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-8">
+            <div className="flex flex-col gap-4">
+              <h4>Ressources</h4>
+              <nav className="flex flex-wrap gap-4 ">
+                <Link href="/activities/events">Evenements</Link>
+                <Link href="/activities/organisers">Organisateurs</Link>
+                <Link href="/cities">Villes</Link>
+                <Link href="/saved/inscriptions">Mes inscriptions</Link>
+                <Link href="/saved/marked">Marqués</Link>
+                <Link href="/subscriptions">Abonnements</Link>
+              </nav>
+            </div>
+            <div className="flex flex-col gap-4">
+              <h4>Gérer son compte</h4>
+              <nav className="flex flex-wrap gap-4 ">
+                <Link href="">Informations du compte</Link>
+                <Link href="">Mes evenements</Link>
+                <Link href="">Evenements participés</Link>
+                <Link href="">Avis</Link>
+              </nav>
+            </div>
           </div>
         </div>
         <p className="border-t border-[var(--secondary-border-col)] pt-3">
