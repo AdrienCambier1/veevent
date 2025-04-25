31e2a6e427bf10b9ab1527623efb4488ec7a151e
Cambier-Adrien <adrien.cambier04@gmail.com>
2025-04-25 22:57:05 +0200

    ...

--- src/components/cards/ticket-card.jsx
+++ src/components/cards/ticket-card.jsx
@@ -3,11 +3,9 @@ import { Plus } from "iconoir-react";
 export default function TicketCard({ title, description, price, onClick }) {
   return (
     <div className="ticket-card">
-      <div className="corner left-0 top-0 -translate-x-1/2 -translate-y-1/2" />
-      <div className="corner right-0 top-0 translate-x-1/2 -translate-y-1/2" />
-      <div className="corner right-0 bottom-0 translate-x-1/2 translate-y-1/2" />
-      <div className="corner left-0 bottom-0 -translate-x-1/2 translate-y-1/2" />
-      <div className="dark-gradient p-6 flex flex-col gap-4 justify-between">
+      <div className="bg-circle h-60 w-60 top-0 -translate-y-1/2 -translate-x-1/2 left-0" />
+      <div className="bg-circle h-32 w-32 bottom-0 -translate-x-1/2 left-0" />
+      <div className="dark-gradient p-6 flex flex-col gap-6 justify-between">
         <div className="flex flex-col gap-4">
           <h3>{title}</h3>
           <p>{description}</p>
