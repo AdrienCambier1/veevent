5f5fd054b751194e6b0f23a087701b143c1bff08
Cambier-Adrien <adrien.cambier04@gmail.com>
2025-05-20 11:25:09 +0200

    ...

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,6 +8,18 @@ import { CityProvider } from "@/contexts/city-context";
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+        <meta name="theme-color" content="transparent" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+      </head>
       <body>
         <AuthProvider>
           <CityProvider>
