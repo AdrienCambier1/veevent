68375ecc1647b76ce3deeb425a8b938109c3871c
Cambier-Adrien <adrien.cambier04@gmail.com>
2025-05-20 11:40:23 +0200

    ...

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,26 +1,26 @@
-"use client";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { AuthProvider } from "@/contexts/auth-context";
 import { CityProvider } from "@/contexts/city-context";
-import Head from "next/head";
+
+export const metadata = {
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "black-translucent",
+  },
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  themeColor: "transparent",
+  viewportFit: "cover",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, viewport-fit=cover"
-        />
-        <meta name="theme-color" content="transparent" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        />
-      </Head>
       <body>
         <AuthProvider>
           <CityProvider>
