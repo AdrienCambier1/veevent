6f70f0d83fe9430e3c45f4dd40656b75fa137d4a
Jean-Baptiste Sainte-Beuve <saintebeuve@gmail.com>
2025-05-31 13:23:30 +0200

    Refactor header component and styles; add logo SVG and enhance layout with city selector

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,39 @@
 import Link from "next/link";
+import logo from "@/assets/images/veevent.svg";
+import Image from "next/image";
+import { City, NavArrowDown, NavArrowLeft } from "iconoir-react";
+import "@/assets/styles/header.scss";
 
-export default function Header() {
+export default function Header({ hideCitySelector = false }) {
   return (
     <header>
-      <Link href="/" className="logo">
-        v<span>ee</span>vent
-      </Link>
+      <div className="left-column">
+        {hideCitySelector ? (
+          <Link href="javascript:history.back()">
+            <div className="back-button"><NavArrowLeft />Retour</div>
+          </Link>
+        ) : (
+          <Link href="/" className="logo">
+            <Image src={logo} alt={'Veevent Logo'} />
+          </Link>
+        )}
+      </div>
+
+      <div className="middle-column">
+        {hideCitySelector && (
+          <Link href="/" className="logo">
+            <Image src={logo} alt={'Veevent Logo'} />
+          </Link>
+        )}
+      </div>
+
+      <div className="right-column">
+        {!hideCitySelector && (
+          <div className="city-selector">
+            <City strokeWidth={2} /><span>Nice<NavArrowDown className={`text-xs`} /></span>
+          </div>
+        )}
+      </div>
     </header>
   );
 }
