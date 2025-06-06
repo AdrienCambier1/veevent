38004f171dfde468758058113fc705cee5b86a38
Jean-Baptiste Sainte-Beuve <saintebeuve@gmail.com>
2025-06-06 18:43:32 +0200

    add theme color for ios

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,14 @@ import { CityProvider } from "@/contexts/city-context";
 import { SidebarProvider } from "@/contexts/sidebar-context";
 import { ReactNode } from "react";
 import FloatingMenu from "@/components/menu/floating-menu/floating-menu";
+import { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   appleWebApp: {
     capable: true,
     statusBarStyle: "black-translucent" as const,
   },
+  themeColor: "#f7f7f7",
 };
 
 export const viewport = {
