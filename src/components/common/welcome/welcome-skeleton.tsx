c160b8cc32e13568285f29736b4b16c574c43b63
Cambier-Adrien <adrien.cambier04@gmail.com>
2025-06-19 19:04:15 +0200

    feat: update skeleton styles for consistency and clarity in welcome component

--- src/components/common/welcome/welcome-skeleton.tsx
+++ src/components/common/welcome/welcome-skeleton.tsx
@@ -1,8 +1,10 @@
 export default function WelcomeSkeleton() {
   return (
     <div className="welcome">
-      <p className="hello skeleton">Bonjour visiteur</p>
-      <p className="welcome-links skeleton">Connectez-vous ou inscrivez-vous</p>
+      <p className="hello skeleton-text">Bonjour visiteur</p>
+      <p className="welcome-links skeleton-text">
+        Connectez-vous ou inscrivez-vous
+      </p>
     </div>
   );
 }
